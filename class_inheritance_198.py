dcd1e167e65188c651fa25c33ba55688e69d3ff0
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2020-12-29 08:58:42 -0700

    push to main

---
+++ class_inheritance_198.py
@@ -0,0 +1,19 @@
+#Created an initial base class 
+class Person:    
+    fname =    #defining attributes
+    lname =    #defining attributes
+    Age =      #defining attributes
+    DoB =      #defining attributes
+
+  
+# defining another class with inherited "person" class attributes
+class Student(person)
+    studentID = 
+    studentstatus= 
+  
+  
+class Resident(Person, Student): # extends both Person and Student class  
+    BuildingNumber =
+    AccessKeyCode = 
+  
+ 
