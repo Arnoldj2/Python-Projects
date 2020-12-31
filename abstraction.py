68f86a40d01acb6aa9d79ad5bf31d7ec928787ee
Arnoldj2 <74551278+Arnoldj2@users.noreply.github.com>
2020-12-31 13:37:46 -0700

    push to main

--- abstraction.py
+++ abstraction.py
@@ -5,24 +5,42 @@ from abc import abstractmethod, ABC
 with ABC and include any abstraction methods,then the classes inherited
 from this class will have to mandatorily implement those abstract methods. """
 
-class Shape(ABC):   # Here, Shape is a parent inherited from ABC class. It has an abstraction method "area"
+class Shape(ABC):   # Here, Shape is a parent inherited from ABC class. It has an abstraction method "area" and "perimeter"
+    
     
     @abstractmethod  #  method is annotated with an abstractmethod keyword
     def area(self): pass
 
+    @abstractmethod
     def perimeter(self): pass
 
+    def attribute(self):  # method without abstractmethod keyword
+        msg = "unknown attribute"
+        return msg
 
-class Square(Shape):  # Square is another class that is inherited from Shape , so it has to implement the drive method.
+
+class Square(Shape): # Square is another class that is inherited from Shape , so it has to implement the area and perimeter method.
+    
     def __init__(self, side):
         self.side = side
         
-""" if i were to add in the following
-
- def area(self):
+    def area(self):  # here we define the implimentation of the abstract methods
         return self.side * self.side
+    
+    def perimeter(self): # here we define the implimentation of the abstract methods
+        return 4 * self.side
 
-The code would run without any problems, because the asbstract class was inherited """
 
 
-square = Square(5)  # Can't instantiate abstract class Square with abstract method area
+if __name__ == "__main__":
+    
+    square = Square(5)
+    print(square.area()) 
+    print(square.perimeter())
+    print(square.attribute())
+
+    
+
+
+    
+ 
